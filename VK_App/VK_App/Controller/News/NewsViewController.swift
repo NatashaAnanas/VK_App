47b8bd4f508dd3eb6a9a3d159d1d1665f9e11cc7
Наташа Ананас <konnat7@mail.ru>
2022-12-14 15:52:28 +0300

    Правки

--- VK_App/VK_App/Controller/News/NewsViewController.swift
+++ VK_App/VK_App/Controller/News/NewsViewController.swift
@@ -6,46 +6,46 @@ import UIKit
 /// Экран новостей
 final class NewsViewController: UIViewController {
     // MARK: - Private Constants
-
+    
     private enum Constants {
         static let newsIDCellText = "news"
         static let newsTextIDCellText = "newsText"
         static let emptyText = ""
     }
-
+    
     // MARK: - Private IBOutlet
-
+    
     @IBOutlet private var newsTableView: UITableView!
-
+    
     // MARK: - Private Visual Components
-
+    
     private let refreshControl: UIRefreshControl = {
         let refresh = UIRefreshControl()
         refresh.tintColor = .white
         return refresh
     }()
-
+    
     // MARK: - Private Properties
-
+    
     private let post = Post()
     private let networkService = NetworkService()
     private var newsFeeds: [NewsFeed] = []
-
+    
     // MARK: - Life cycle
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpRefreshControl()
         fetchNews()
         setupTableView()
     }
-
+    
     // MARK: - Private Methods
-
+    
     private func setupTableView() {
         newsTableView.prefetchDataSource = self
     }
-
+    
     private func setUpRefreshControl() {
         newsTableView.addSubview(refreshControl)
         refreshControl.addTarget(
@@ -54,23 +54,21 @@ final class NewsViewController: UIViewController {
             for: .valueChanged
         )
     }
-
+    
     private func fetchNews() {
         networkService.fetchNews { [weak self] result in
             guard let self = self else { return }
             switch result {
             case let .success(success):
                 self.filterResponse(response: success.response)
-                DispatchQueue.main.async {
-                    self.newsFeeds = success.response.newsFeed + self.newsFeeds
-                    self.newsTableView.reloadData()
-                }
+                self.newsFeeds = success.response.newsFeed + self.newsFeeds
+                self.newsTableView.reloadData()
             case let .failure(failure):
                 print(failure.localizedDescription)
             }
         }
     }
-
+    
     private func filterResponse(response: NewsResponse) {
         response.newsFeed.forEach { item in
             if item.sourceID < 0 {
@@ -79,7 +77,7 @@ final class NewsViewController: UIViewController {
                 }).first else { return }
                 item.authorName = group.groupName
                 item.avatarPath = group.urlPhoto
-
+                
             } else {
                 guard let user = response.friends.filter({ user in
                     user.id == item.sourceID
@@ -88,7 +86,7 @@ final class NewsViewController: UIViewController {
             }
         }
     }
-
+    
     @objc private func refreshControlAction(sender: UIRefreshControl) {
         refreshControl.beginRefreshing()
         fetchNews()
@@ -104,27 +102,27 @@ extension NewsViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_: UITableView, numberOfRowsInSection _: Int) -> Int {
         newsFeeds.count
     }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(
             withIdentifier: Constants.newsIDCellText,
             for: indexPath
         ) as? NewsTableViewCell else { return UITableViewCell() }
-
+        
         let news = newsFeeds[indexPath.row]
-
+        
         cell.configure(
             userNameText: news.authorName ?? Constants.emptyText,
             userImageText: news.avatarPath ?? Constants.emptyText,
             dateText: convert(timeStamp: Int(news.date)),
             descriptionText: news.text,
             postImageText: news.attachments?.first?.photo?.sizes.first?.url
-                ?? Constants.emptyText,
+            ?? Constants.emptyText,
             likes: String(news.likes.count)
         )
         return cell
     }
-
+    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         let newsFeeds = newsFeeds[indexPath.row]
         guard
